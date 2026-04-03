60fd52b7c7038d22708146da6ecedac441c188be
BlueW <badonkadooonkmc@gmail.com>
2026-04-04 00:45:48 +0800

    Optimize: Replace Tesseract.js with lightweight template matching solver

---
+++ package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A simple script to package the extension for end users
+
+echo "Packaging NCKU Moodle Keeper..."
+zip ncku-moodle-keeper.zip manifest.json captcha.js session_keeper.js templates.json README.md
+echo "Done."
