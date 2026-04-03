bf6a1d850dfda106b048c5fb10b54880fe2ab96d
BlueW <badonkadooonkmc@gmail.com>
2026-04-04 01:36:54 +0800

    Release v1.4-beta: Expand support to multiple NCKU systems (Course, FuturePlus) and optimize solver

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@
 # A simple script to package the extension for end users
 
 echo "Packaging NCKU Moodle Keeper..."
-zip ncku-moodle-keeper.zip manifest.json captcha.js session_keeper.js templates.json README.md
+zip ncku-moodle-keeper.zip manifest.json captcha.js session_keeper.js templates.json templates2.json README.md
 echo "Done."
